5940cd9370d605c7ede3d32b7c8432271509f0a1
jacob barrantes <stevenbarrantes33@gmail.com>
2026-03-13 19:59:04 -0600

    Cambio de nombre

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -45,16 +45,18 @@
             this.panel1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(188, 354);
+            this.panel1.Size = new System.Drawing.Size(251, 436);
             this.panel1.TabIndex = 0;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::grupo3_Proyecto.Properties.Resources.LOGOCOLOR;
-            this.pictureBox1.Location = new System.Drawing.Point(7, 115);
+            this.pictureBox1.Location = new System.Drawing.Point(9, 142);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(173, 94);
+            this.pictureBox1.Size = new System.Drawing.Size(231, 116);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -63,9 +65,10 @@
             // 
             this.lblCedula.AutoSize = true;
             this.lblCedula.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCedula.Location = new System.Drawing.Point(218, 61);
+            this.lblCedula.Location = new System.Drawing.Point(291, 75);
+            this.lblCedula.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblCedula.Name = "lblCedula";
-            this.lblCedula.Size = new System.Drawing.Size(46, 33);
+            this.lblCedula.Size = new System.Drawing.Size(56, 42);
             this.lblCedula.TabIndex = 2;
             this.lblCedula.Text = "ID";
             this.lblCedula.Click += new System.EventHandler(this.txtCedula_Click);
@@ -73,35 +76,39 @@
             // txtCedula
             // 
             this.txtCedula.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtCedula.Location = new System.Drawing.Point(413, 56);
+            this.txtCedula.Location = new System.Drawing.Point(551, 69);
+            this.txtCedula.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtCedula.Name = "txtCedula";
-            this.txtCedula.Size = new System.Drawing.Size(388, 38);
+            this.txtCedula.Size = new System.Drawing.Size(516, 46);
             this.txtCedula.TabIndex = 3;
             // 
             // txtContrasena
             // 
             this.txtContrasena.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtContrasena.Location = new System.Drawing.Point(413, 139);
+            this.txtContrasena.Location = new System.Drawing.Point(551, 171);
+            this.txtContrasena.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtContrasena.Name = "txtContrasena";
-            this.txtContrasena.Size = new System.Drawing.Size(388, 38);
+            this.txtContrasena.Size = new System.Drawing.Size(516, 46);
             this.txtContrasena.TabIndex = 5;
             // 
             // lblContrasena
             // 
             this.lblContrasena.AutoSize = true;
             this.lblContrasena.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblContrasena.Location = new System.Drawing.Point(218, 141);
+            this.lblContrasena.Location = new System.Drawing.Point(291, 174);
+            this.lblContrasena.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblContrasena.Name = "lblContrasena";
-            this.lblContrasena.Size = new System.Drawing.Size(151, 33);
+            this.lblContrasena.Size = new System.Drawing.Size(191, 42);
             this.lblContrasena.TabIndex = 4;
             this.lblContrasena.Text = "Password";
             // 
             // btnIngresar
             // 
             this.btnIngresar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnIngresar.Location = new System.Drawing.Point(224, 223);
+            this.btnIngresar.Location = new System.Drawing.Point(299, 274);
+            this.btnIngresar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnIngresar.Name = "btnIngresar";
-            this.btnIngresar.Size = new System.Drawing.Size(225, 43);
+            this.btnIngresar.Size = new System.Drawing.Size(300, 53);
             this.btnIngresar.TabIndex = 6;
             this.btnIngresar.Text = "&Login";
             this.btnIngresar.UseVisualStyleBackColor = true;
@@ -110,20 +117,21 @@
             // btnSalir
             // 
             this.btnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSalir.Location = new System.Drawing.Point(488, 223);
+            this.btnSalir.Location = new System.Drawing.Point(651, 274);
+            this.btnSalir.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnSalir.Name = "btnSalir";
-            this.btnSalir.Size = new System.Drawing.Size(254, 43);
+            this.btnSalir.Size = new System.Drawing.Size(339, 53);
             this.btnSalir.TabIndex = 7;
-            this.btnSalir.Text = "&Exit";
+            this.btnSalir.Text = "&Salir";
             this.btnSalir.UseVisualStyleBackColor = true;
             this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
             // 
             // txtLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
-            this.ClientSize = new System.Drawing.Size(840, 362);
+            this.ClientSize = new System.Drawing.Size(1120, 446);
             this.Controls.Add(this.btnSalir);
             this.Controls.Add(this.btnIngresar);
             this.Controls.Add(this.txtContrasena);
@@ -131,6 +139,7 @@
             this.Controls.Add(this.txtCedula);
             this.Controls.Add(this.lblCedula);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "txtLogin";
             this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
             this.Text = "Login";
