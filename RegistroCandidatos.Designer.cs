756c3c564fddc62600d0a5166256bded4888fd23
Nazareth Jimenez <nazareth.jimenez.godinez@gmail.com>
2026-03-17 21:53:58 -0600

    Cambio de boton salir a menú

---
+++ RegistroCandidatos.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace grupo3_Proyecto
+{
+    partial class frmRegistroCandidatos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmRegistroCandidatos));
+            this.btnSalirCredenciales = new System.Windows.Forms.Button();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.txtContraseñaNueva = new System.Windows.Forms.TextBox();
+            this.lblContraseñaNueva = new System.Windows.Forms.Label();
+            this.lblUsuarioNuevo = new System.Windows.Forms.Label();
+            this.txtUsuarioNuevo = new System.Windows.Forms.TextBox();
+            this.lblCredenciales = new System.Windows.Forms.Label();
+            this.pctContraseña = new System.Windows.Forms.PictureBox();
+            this.pctUsuario = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pctContraseña)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctUsuario)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSalirCredenciales
+            // 
+            this.btnSalirCredenciales.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalirCredenciales.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalirCredenciales.Location = new System.Drawing.Point(439, 298);
+            this.btnSalirCredenciales.Name = "btnSalirCredenciales";
+            this.btnSalirCredenciales.Size = new System.Drawing.Size(121, 45);
+            this.btnSalirCredenciales.TabIndex = 98;
+            this.btnSalirCredenciales.Text = "Menú";
+            this.btnSalirCredenciales.UseVisualStyleBackColor = true;
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnRegistrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRegistrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegistrar.Location = new System.Drawing.Point(256, 298);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(121, 45);
+            this.btnRegistrar.TabIndex = 97;
+            this.btnRegistrar.Text = "Registrar";
+            this.btnRegistrar.UseVisualStyleBackColor = false;
+            // 
+            // txtContraseñaNueva
+            // 
+            this.txtContraseñaNueva.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtContraseñaNueva.Location = new System.Drawing.Point(267, 214);
+            this.txtContraseñaNueva.Name = "txtContraseñaNueva";
+            this.txtContraseñaNueva.Size = new System.Drawing.Size(479, 26);
+            this.txtContraseñaNueva.TabIndex = 94;
+            // 
+            // lblContraseñaNueva
+            // 
+            this.lblContraseñaNueva.AutoSize = true;
+            this.lblContraseñaNueva.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblContraseñaNueva.Location = new System.Drawing.Point(120, 215);
+            this.lblContraseñaNueva.Name = "lblContraseñaNueva";
+            this.lblContraseñaNueva.Size = new System.Drawing.Size(123, 25);
+            this.lblContraseñaNueva.TabIndex = 93;
+            this.lblContraseñaNueva.Text = "Contraseña";
+            // 
+            // lblUsuarioNuevo
+            // 
+            this.lblUsuarioNuevo.AutoSize = true;
+            this.lblUsuarioNuevo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsuarioNuevo.Location = new System.Drawing.Point(120, 124);
+            this.lblUsuarioNuevo.Name = "lblUsuarioNuevo";
+            this.lblUsuarioNuevo.Size = new System.Drawing.Size(86, 25);
+            this.lblUsuarioNuevo.TabIndex = 92;
+            this.lblUsuarioNuevo.Text = "Usuario";
+            // 
+            // txtUsuarioNuevo
+            // 
+            this.txtUsuarioNuevo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsuarioNuevo.Location = new System.Drawing.Point(267, 123);
+            this.txtUsuarioNuevo.Name = "txtUsuarioNuevo";
+            this.txtUsuarioNuevo.Size = new System.Drawing.Size(479, 26);
+            this.txtUsuarioNuevo.TabIndex = 91;
+            // 
+            // lblCredenciales
+            // 
+            this.lblCredenciales.AutoSize = true;
+            this.lblCredenciales.Font = new System.Drawing.Font("Modern No. 20", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCredenciales.Location = new System.Drawing.Point(180, 29);
+            this.lblCredenciales.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblCredenciales.Name = "lblCredenciales";
+            this.lblCredenciales.Size = new System.Drawing.Size(378, 36);
+            this.lblCredenciales.TabIndex = 90;
+            this.lblCredenciales.Text = "Ingrese nuevo candidato";
+            this.lblCredenciales.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // pctContraseña
+            // 
+            this.pctContraseña.Image = ((System.Drawing.Image)(resources.GetObject("pctContraseña.Image")));
+            this.pctContraseña.Location = new System.Drawing.Point(55, 200);
+            this.pctContraseña.Name = "pctContraseña";
+            this.pctContraseña.Size = new System.Drawing.Size(46, 50);
+            this.pctContraseña.TabIndex = 96;
+            this.pctContraseña.TabStop = false;
+            // 
+            // pctUsuario
+            // 
+            this.pctUsuario.Image = ((System.Drawing.Image)(resources.GetObject("pctUsuario.Image")));
+            this.pctUsuario.Location = new System.Drawing.Point(55, 112);
+            this.pctUsuario.Name = "pctUsuario";
+            this.pctUsuario.Size = new System.Drawing.Size(46, 50);
+            this.pctUsuario.TabIndex = 95;
+            this.pctUsuario.TabStop = false;
+            // 
+            // frmRegistroCandidatos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 372);
+            this.Controls.Add(this.btnSalirCredenciales);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.pctContraseña);
+            this.Controls.Add(this.pctUsuario);
+            this.Controls.Add(this.txtContraseñaNueva);
+            this.Controls.Add(this.lblContraseñaNueva);
+            this.Controls.Add(this.lblUsuarioNuevo);
+            this.Controls.Add(this.txtUsuarioNuevo);
+            this.Controls.Add(this.lblCredenciales);
+            this.Name = "frmRegistroCandidatos";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Registro de Candidatos";
+            ((System.ComponentModel.ISupportInitialize)(this.pctContraseña)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctUsuario)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSalirCredenciales;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.PictureBox pctContraseña;
+        private System.Windows.Forms.PictureBox pctUsuario;
+        private System.Windows.Forms.TextBox txtContraseñaNueva;
+        private System.Windows.Forms.Label lblContraseñaNueva;
+        private System.Windows.Forms.Label lblUsuarioNuevo;
+        private System.Windows.Forms.TextBox txtUsuarioNuevo;
+        private System.Windows.Forms.Label lblCredenciales;
+    }
+}
