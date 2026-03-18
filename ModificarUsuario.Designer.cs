06c27258d51be49cea25c3b8e7ba066c8e00afd7
Nazareth Jimenez <nazareth.jimenez.godinez@gmail.com>
2026-03-17 22:24:39 -0600

    Creacion de form de modificar usuario

---
+++ ModificarUsuario.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace grupo3_Proyecto
+{
+    partial class ModificarUsuario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ModificarUsuario));
+            this.btnAtrásModificar = new System.Windows.Forms.Button();
+            this.btnModificarUsuario = new System.Windows.Forms.Button();
+            this.lblUsuarioAnterior = new System.Windows.Forms.Label();
+            this.txtUsuarioAnterior = new System.Windows.Forms.TextBox();
+            this.txtModificarUsuario = new System.Windows.Forms.Label();
+            this.pctUsuario = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblUsuarioModificado = new System.Windows.Forms.Label();
+            this.txtUsuarioModificado = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pctUsuario)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAtrásModificar
+            // 
+            this.btnAtrásModificar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAtrásModificar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAtrásModificar.Location = new System.Drawing.Point(429, 348);
+            this.btnAtrásModificar.Name = "btnAtrásModificar";
+            this.btnAtrásModificar.Size = new System.Drawing.Size(121, 45);
+            this.btnAtrásModificar.TabIndex = 118;
+            this.btnAtrásModificar.Text = "Atrás";
+            this.btnAtrásModificar.UseVisualStyleBackColor = true;
+            // 
+            // btnModificarUsuario
+            // 
+            this.btnModificarUsuario.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnModificarUsuario.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnModificarUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnModificarUsuario.Location = new System.Drawing.Point(246, 348);
+            this.btnModificarUsuario.Name = "btnModificarUsuario";
+            this.btnModificarUsuario.Size = new System.Drawing.Size(121, 45);
+            this.btnModificarUsuario.TabIndex = 117;
+            this.btnModificarUsuario.Text = "Modificar";
+            this.btnModificarUsuario.UseVisualStyleBackColor = false;
+            // 
+            // lblUsuarioAnterior
+            // 
+            this.lblUsuarioAnterior.AutoSize = true;
+            this.lblUsuarioAnterior.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsuarioAnterior.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblUsuarioAnterior.Location = new System.Drawing.Point(116, 159);
+            this.lblUsuarioAnterior.Name = "lblUsuarioAnterior";
+            this.lblUsuarioAnterior.Size = new System.Drawing.Size(165, 25);
+            this.lblUsuarioAnterior.TabIndex = 115;
+            this.lblUsuarioAnterior.Text = "Usuario anterior";
+            // 
+            // txtUsuarioAnterior
+            // 
+            this.txtUsuarioAnterior.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsuarioAnterior.Location = new System.Drawing.Point(296, 160);
+            this.txtUsuarioAnterior.Name = "txtUsuarioAnterior";
+            this.txtUsuarioAnterior.Size = new System.Drawing.Size(453, 26);
+            this.txtUsuarioAnterior.TabIndex = 114;
+            // 
+            // txtModificarUsuario
+            // 
+            this.txtModificarUsuario.AutoSize = true;
+            this.txtModificarUsuario.Font = new System.Drawing.Font("Modern No. 20", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtModificarUsuario.Location = new System.Drawing.Point(256, 49);
+            this.txtModificarUsuario.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.txtModificarUsuario.Name = "txtModificarUsuario";
+            this.txtModificarUsuario.Size = new System.Drawing.Size(294, 36);
+            this.txtModificarUsuario.TabIndex = 113;
+            this.txtModificarUsuario.Text = "Modificar Usuario";
+            // 
+            // pctUsuario
+            // 
+            this.pctUsuario.Image = ((System.Drawing.Image)(resources.GetObject("pctUsuario.Image")));
+            this.pctUsuario.Location = new System.Drawing.Point(54, 144);
+            this.pctUsuario.Name = "pctUsuario";
+            this.pctUsuario.Size = new System.Drawing.Size(46, 50);
+            this.pctUsuario.TabIndex = 116;
+            this.pctUsuario.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(54, 230);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(46, 50);
+            this.pictureBox1.TabIndex = 121;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblUsuarioModificado
+            // 
+            this.lblUsuarioModificado.AutoSize = true;
+            this.lblUsuarioModificado.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsuarioModificado.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblUsuarioModificado.Location = new System.Drawing.Point(116, 245);
+            this.lblUsuarioModificado.Name = "lblUsuarioModificado";
+            this.lblUsuarioModificado.Size = new System.Drawing.Size(154, 25);
+            this.lblUsuarioModificado.TabIndex = 120;
+            this.lblUsuarioModificado.Text = "Nuevo Usuario";
+            // 
+            // txtUsuarioModificado
+            // 
+            this.txtUsuarioModificado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsuarioModificado.Location = new System.Drawing.Point(296, 246);
+            this.txtUsuarioModificado.Name = "txtUsuarioModificado";
+            this.txtUsuarioModificado.Size = new System.Drawing.Size(453, 26);
+            this.txtUsuarioModificado.TabIndex = 119;
+            // 
+            // ModificarUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblUsuarioModificado);
+            this.Controls.Add(this.txtUsuarioModificado);
+            this.Controls.Add(this.btnAtrásModificar);
+            this.Controls.Add(this.btnModificarUsuario);
+            this.Controls.Add(this.pctUsuario);
+            this.Controls.Add(this.lblUsuarioAnterior);
+            this.Controls.Add(this.txtUsuarioAnterior);
+            this.Controls.Add(this.txtModificarUsuario);
+            this.Name = "ModificarUsuario";
+            this.Text = "ModificarUsuario";
+            ((System.ComponentModel.ISupportInitialize)(this.pctUsuario)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAtrásModificar;
+        private System.Windows.Forms.Button btnModificarUsuario;
+        private System.Windows.Forms.PictureBox pctUsuario;
+        private System.Windows.Forms.Label lblUsuarioAnterior;
+        private System.Windows.Forms.TextBox txtUsuarioAnterior;
+        private System.Windows.Forms.Label txtModificarUsuario;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblUsuarioModificado;
+        private System.Windows.Forms.TextBox txtUsuarioModificado;
+    }
+}
