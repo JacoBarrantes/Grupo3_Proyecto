48110ff9744407ce9732ffbe0c2635e25eafdde4
jacob barrantes <stevenbarrantes33@gmail.com>
2026-04-14 20:35:14 -0600

    Remodelacion de forms

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,22 +35,8 @@
             this.btnIngresar = new System.Windows.Forms.Button();
             this.btnSalir = new System.Windows.Forms.Button();
             this.btnRegistrar = new System.Windows.Forms.Button();
-            this.lblNombre = new System.Windows.Forms.Label();
-            this.txtNombre = new System.Windows.Forms.TextBox();
-            this.txtApellidos = new System.Windows.Forms.TextBox();
-            this.lblApellidos = new System.Windows.Forms.Label();
-            this.dtpVencimiento = new System.Windows.Forms.DateTimePicker();
-            this.lblVencimiento = new System.Windows.Forms.Label();
-            this.cmbProvincia = new System.Windows.Forms.ComboBox();
-            this.lblProvincia = new System.Windows.Forms.Label();
-            this.lblCanton = new System.Windows.Forms.Label();
-            this.cmbCanton = new System.Windows.Forms.ComboBox();
-            this.lblDistrito = new System.Windows.Forms.Label();
-            this.cmbDistrito = new System.Windows.Forms.ComboBox();
-            this.txtCorreo = new System.Windows.Forms.TextBox();
-            this.lblCorreo = new System.Windows.Forms.Label();
-            this.cmbContrasena = new System.Windows.Forms.TextBox();
             this.lblContrasena = new System.Windows.Forms.Label();
+            this.txtContrasena = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -59,28 +45,32 @@
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Location = new System.Drawing.Point(0, -25);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(188, 510);
+            this.panel1.Size = new System.Drawing.Size(251, 515);
             this.panel1.TabIndex = 0;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::grupo3_Proyecto.Properties.Resources.LOGOCOLOR;
-            this.pictureBox1.Location = new System.Drawing.Point(3, 251);
+            this.pictureBox1.Location = new System.Drawing.Point(13, 217);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(173, 94);
+            this.pictureBox1.Size = new System.Drawing.Size(231, 116);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // lblCedula
             // 
             this.lblCedula.AutoSize = true;
             this.lblCedula.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCedula.Location = new System.Drawing.Point(222, 31);
+            this.lblCedula.Location = new System.Drawing.Point(282, 119);
+            this.lblCedula.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblCedula.Name = "lblCedula";
-            this.lblCedula.Size = new System.Drawing.Size(65, 20);
+            this.lblCedula.Size = new System.Drawing.Size(81, 25);
             this.lblCedula.TabIndex = 2;
             this.lblCedula.Text = "Cédula";
             this.lblCedula.Click += new System.EventHandler(this.txtCedula_Click);
@@ -88,17 +78,19 @@
             // txtCedula
             // 
             this.txtCedula.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtCedula.Location = new System.Drawing.Point(352, 28);
+            this.txtCedula.Location = new System.Drawing.Point(455, 115);
+            this.txtCedula.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtCedula.Name = "txtCedula";
-            this.txtCedula.Size = new System.Drawing.Size(331, 26);
+            this.txtCedula.Size = new System.Drawing.Size(440, 30);
             this.txtCedula.TabIndex = 3;
             // 
             // btnIngresar
             // 
             this.btnIngresar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnIngresar.Location = new System.Drawing.Point(227, 467);
+            this.btnIngresar.Location = new System.Drawing.Point(270, 286);
+            this.btnIngresar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnIngresar.Name = "btnIngresar";
-            this.btnIngresar.Size = new System.Drawing.Size(225, 43);
+            this.btnIngresar.Size = new System.Drawing.Size(300, 53);
             this.btnIngresar.TabIndex = 6;
             this.btnIngresar.Text = "&Login";
             this.btnIngresar.UseVisualStyleBackColor = true;
@@ -107,9 +99,10 @@
             // btnSalir
             // 
             this.btnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSalir.Location = new System.Drawing.Point(723, 561);
+            this.btnSalir.Location = new System.Drawing.Point(909, 368);
+            this.btnSalir.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnSalir.Name = "btnSalir";
-            this.btnSalir.Size = new System.Drawing.Size(75, 34);
+            this.btnSalir.Size = new System.Drawing.Size(100, 42);
             this.btnSalir.TabIndex = 7;
             this.btnSalir.Text = "&Salir";
             this.btnSalir.UseVisualStyleBackColor = true;
@@ -118,188 +111,50 @@
             // btnRegistrar
             // 
             this.btnRegistrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRegistrar.Location = new System.Drawing.Point(469, 467);
-            this.btnRegistrar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnRegistrar.Location = new System.Drawing.Point(592, 286);
+            this.btnRegistrar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnRegistrar.Name = "btnRegistrar";
-            this.btnRegistrar.Size = new System.Drawing.Size(227, 43);
+            this.btnRegistrar.Size = new System.Drawing.Size(303, 53);
             this.btnRegistrar.TabIndex = 90;
             this.btnRegistrar.Text = "&Registrarse";
             this.btnRegistrar.UseVisualStyleBackColor = true;
             this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
             // 
-            // lblNombre
-            // 
-            this.lblNombre.AutoSize = true;
-            this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNombre.Location = new System.Drawing.Point(222, 81);
-            this.lblNombre.Name = "lblNombre";
-            this.lblNombre.Size = new System.Drawing.Size(71, 20);
-            this.lblNombre.TabIndex = 91;
-            this.lblNombre.Text = "Nombre";
-            // 
-            // txtNombre
-            // 
-            this.txtNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtNombre.Location = new System.Drawing.Point(352, 72);
-            this.txtNombre.Name = "txtNombre";
-            this.txtNombre.ReadOnly = true;
-            this.txtNombre.Size = new System.Drawing.Size(331, 26);
-            this.txtNombre.TabIndex = 92;
-            // 
-            // txtApellidos
-            // 
-            this.txtApellidos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtApellidos.Location = new System.Drawing.Point(352, 122);
-            this.txtApellidos.Name = "txtApellidos";
-            this.txtApellidos.ReadOnly = true;
-            this.txtApellidos.Size = new System.Drawing.Size(331, 26);
-            this.txtApellidos.TabIndex = 94;
-            // 
-            // lblApellidos
-            // 
-            this.lblApellidos.AutoSize = true;
-            this.lblApellidos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblApellidos.Location = new System.Drawing.Point(222, 131);
-            this.lblApellidos.Name = "lblApellidos";
-            this.lblApellidos.Size = new System.Drawing.Size(82, 20);
-            this.lblApellidos.TabIndex = 93;
-            this.lblApellidos.Text = "Apellidos";
-            // 
-            // dtpVencimiento
-            // 
-            this.dtpVencimiento.Location = new System.Drawing.Point(352, 174);
-            this.dtpVencimiento.Name = "dtpVencimiento";
-            this.dtpVencimiento.Size = new System.Drawing.Size(221, 20);
-            this.dtpVencimiento.TabIndex = 95;
-            // 
-            // lblVencimiento
-            // 
-            this.lblVencimiento.AutoSize = true;
-            this.lblVencimiento.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblVencimiento.Location = new System.Drawing.Point(222, 175);
-            this.lblVencimiento.Name = "lblVencimiento";
-            this.lblVencimiento.Size = new System.Drawing.Size(108, 20);
-            this.lblVencimiento.TabIndex = 96;
-            this.lblVencimiento.Text = "Vencimiento";
-            // 
-            // cmbProvincia
-            // 
-            this.cmbProvincia.FormattingEnabled = true;
-            this.cmbProvincia.Location = new System.Drawing.Point(352, 220);
-            this.cmbProvincia.Name = "cmbProvincia";
-            this.cmbProvincia.Size = new System.Drawing.Size(221, 21);
-            this.cmbProvincia.TabIndex = 97;
-            // 
-            // lblProvincia
-            // 
-            this.lblProvincia.AutoSize = true;
-            this.lblProvincia.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblProvincia.Location = new System.Drawing.Point(223, 221);
-            this.lblProvincia.Name = "lblProvincia";
-            this.lblProvincia.Size = new System.Drawing.Size(81, 20);
-            this.lblProvincia.TabIndex = 98;
-            this.lblProvincia.Text = "Provincia";
-            // 
-            // lblCanton
-            // 
-            this.lblCanton.AutoSize = true;
-            this.lblCanton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCanton.Location = new System.Drawing.Point(223, 264);
-            this.lblCanton.Name = "lblCanton";
-            this.lblCanton.Size = new System.Drawing.Size(67, 20);
-            this.lblCanton.TabIndex = 100;
-            this.lblCanton.Text = "Cantón";
-            // 
-            // cmbCanton
-            // 
-            this.cmbCanton.FormattingEnabled = true;
-            this.cmbCanton.Location = new System.Drawing.Point(352, 263);
-            this.cmbCanton.Name = "cmbCanton";
-            this.cmbCanton.Size = new System.Drawing.Size(221, 21);
-            this.cmbCanton.TabIndex = 99;
-            // 
-            // lblDistrito
-            // 
-            this.lblDistrito.AutoSize = true;
-            this.lblDistrito.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDistrito.Location = new System.Drawing.Point(223, 308);
-            this.lblDistrito.Name = "lblDistrito";
-            this.lblDistrito.Size = new System.Drawing.Size(67, 20);
-            this.lblDistrito.TabIndex = 102;
-            this.lblDistrito.Text = "Distrito";
-            // 
-            // cmbDistrito
-            // 
-            this.cmbDistrito.FormattingEnabled = true;
-            this.cmbDistrito.Location = new System.Drawing.Point(352, 307);
-            this.cmbDistrito.Name = "cmbDistrito";
-            this.cmbDistrito.Size = new System.Drawing.Size(221, 21);
-            this.cmbDistrito.TabIndex = 101;
-            // 
-            // txtCorreo
-            // 
-            this.txtCorreo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtCorreo.Location = new System.Drawing.Point(352, 351);
-            this.txtCorreo.Name = "txtCorreo";
-            this.txtCorreo.Size = new System.Drawing.Size(331, 26);
-            this.txtCorreo.TabIndex = 104;
-            // 
-            // lblCorreo
-            // 
-            this.lblCorreo.AutoSize = true;
-            this.lblCorreo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCorreo.Location = new System.Drawing.Point(222, 354);
-            this.lblCorreo.Name = "lblCorreo";
-            this.lblCorreo.Size = new System.Drawing.Size(63, 20);
-            this.lblCorreo.TabIndex = 103;
-            this.lblCorreo.Text = "Correo";
-            // 
-            // cmbContrasena
-            // 
-            this.cmbContrasena.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cmbContrasena.Location = new System.Drawing.Point(352, 399);
-            this.cmbContrasena.Name = "cmbContrasena";
-            this.cmbContrasena.Size = new System.Drawing.Size(331, 26);
-            this.cmbContrasena.TabIndex = 106;
-            // 
             // lblContrasena
             // 
             this.lblContrasena.AutoSize = true;
             this.lblContrasena.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblContrasena.Location = new System.Drawing.Point(222, 402);
+            this.lblContrasena.Location = new System.Drawing.Point(282, 196);
+            this.lblContrasena.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblContrasena.Name = "lblContrasena";
-            this.lblContrasena.Size = new System.Drawing.Size(102, 20);
+            this.lblContrasena.Size = new System.Drawing.Size(124, 25);
             this.lblContrasena.TabIndex = 105;
             this.lblContrasena.Text = "Contraseña";
             // 
+            // txtContrasena
+            // 
+            this.txtContrasena.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtContrasena.Location = new System.Drawing.Point(444, 193);
+            this.txtContrasena.Margin = new System.Windows.Forms.Padding(4);
+            this.txtContrasena.Name = "txtContrasena";
+            this.txtContrasena.Size = new System.Drawing.Size(440, 30);
+            this.txtContrasena.TabIndex = 106;
+            // 
             // frmLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
-            this.ClientSize = new System.Drawing.Size(822, 642);
-            this.Controls.Add(this.cmbContrasena);
+            this.ClientSize = new System.Drawing.Size(1040, 488);
+            this.Controls.Add(this.txtContrasena);
             this.Controls.Add(this.lblContrasena);
-            this.Controls.Add(this.txtCorreo);
-            this.Controls.Add(this.lblCorreo);
-            this.Controls.Add(this.lblDistrito);
-            this.Controls.Add(this.cmbDistrito);
-            this.Controls.Add(this.lblCanton);
-            this.Controls.Add(this.cmbCanton);
-            this.Controls.Add(this.lblProvincia);
-            this.Controls.Add(this.cmbProvincia);
-            this.Controls.Add(this.lblVencimiento);
-            this.Controls.Add(this.dtpVencimiento);
-            this.Controls.Add(this.txtApellidos);
-            this.Controls.Add(this.lblApellidos);
-            this.Controls.Add(this.txtNombre);
-            this.Controls.Add(this.lblNombre);
             this.Controls.Add(this.btnRegistrar);
             this.Controls.Add(this.btnSalir);
             this.Controls.Add(this.btnIngresar);
             this.Controls.Add(this.txtCedula);
             this.Controls.Add(this.lblCedula);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmLogin";
             this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
             this.Text = "Login";
@@ -320,22 +175,8 @@
         private System.Windows.Forms.Button btnIngresar;
         private System.Windows.Forms.Button btnSalir;
         private System.Windows.Forms.Button btnRegistrar;
-        private System.Windows.Forms.Label lblNombre;
-        private System.Windows.Forms.TextBox txtNombre;
-        private System.Windows.Forms.TextBox txtApellidos;
-        private System.Windows.Forms.Label lblApellidos;
-        private System.Windows.Forms.DateTimePicker dtpVencimiento;
-        private System.Windows.Forms.Label lblVencimiento;
-        private System.Windows.Forms.ComboBox cmbProvincia;
-        private System.Windows.Forms.Label lblProvincia;
-        private System.Windows.Forms.Label lblCanton;
-        private System.Windows.Forms.ComboBox cmbCanton;
-        private System.Windows.Forms.Label lblDistrito;
-        private System.Windows.Forms.ComboBox cmbDistrito;
-        private System.Windows.Forms.TextBox txtCorreo;
-        private System.Windows.Forms.Label lblCorreo;
-        private System.Windows.Forms.TextBox cmbContrasena;
         private System.Windows.Forms.Label lblContrasena;
+        private System.Windows.Forms.TextBox txtContrasena;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MyLibreriaVoto;
 
 namespace grupo3_Proyecto
 {
@@ -43,10 +44,38 @@ namespace grupo3_Proyecto
 
         private void btnIngresar_Click(object sender, EventArgs e)
         {
-            frmMenuOpciones menuOpciones = new frmMenuOpciones();
-            menuOpciones.ShowDialog();
+            
+            IniciarSesion();          
 
 
+        }
+
+       private void IniciarSesion()
+        {
+            try
+            {
+                string cedula = txtCedula.Text.Trim();
+                string contrasena = Utilidades.codificar(txtContrasena.Text.Trim());
+                string cmd = string.Format(
+                    "SELECT * FROM Usuarios WHERE Cedula = '{0}' AND PasswordHash = '{1}'",
+                    cedula, contrasena
+                );
+                DataSet ds = Utilidades.ejecutar(cmd);
+                if (contrasena == txtContrasena.Text.Trim())
+                {
+                    MessageBox.Show("Bienvenido al sistema de votación");
+                    frmMenuOpciones menuOpciones = new frmMenuOpciones();
+                    menuOpciones.ShowDialog();
+                }
+                else
+                {
+                    MessageBox.Show("Cédula o contraseña incorrecta", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al iniciar sesión: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
 
@@ -69,6 +98,11 @@ namespace grupo3_Proyecto
         {
             frmCredenciales credenciales = new frmCredenciales();
             credenciales.ShowDialog();
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
 
         }
     }
