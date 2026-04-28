0f9783775437036fdc25de1873345beae5a34b01
Nazareth Jimenez <nazareth.jimenez.godinez@gmail.com>
2026-04-27 22:16:10 -0600

    creacion del form tipo Eleccion

---
+++ TipoEleccion.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace grupo3_Proyecto
+{
+    partial class TipoEleccion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTipoEleccion = new System.Windows.Forms.Label();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.dgvTipoEleccion = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTipoEleccion)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(51, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Descripción";
+            // 
+            // txtTipoEleccion
+            // 
+            this.txtTipoEleccion.AutoSize = true;
+            this.txtTipoEleccion.Font = new System.Drawing.Font("Modern No. 20", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTipoEleccion.Location = new System.Drawing.Point(254, 35);
+            this.txtTipoEleccion.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.txtTipoEleccion.Name = "txtTipoEleccion";
+            this.txtTipoEleccion.Size = new System.Drawing.Size(268, 36);
+            this.txtTipoEleccion.TabIndex = 114;
+            this.txtTipoEleccion.Text = "Tipo de Elección";
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(193, 111);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(529, 20);
+            this.txtDescripcion.TabIndex = 115;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.Location = new System.Drawing.Point(428, 363);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(121, 45);
+            this.btnEliminar.TabIndex = 117;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnGuardar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.Location = new System.Drawing.Point(55, 363);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(121, 45);
+            this.btnGuardar.TabIndex = 116;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = false;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEditar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditar.Location = new System.Drawing.Point(239, 363);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(121, 45);
+            this.btnEditar.TabIndex = 118;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLimpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.Location = new System.Drawing.Point(601, 363);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(121, 45);
+            this.btnLimpiar.TabIndex = 119;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // dgvTipoEleccion
+            // 
+            this.dgvTipoEleccion.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dgvTipoEleccion.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTipoEleccion.Location = new System.Drawing.Point(55, 160);
+            this.dgvTipoEleccion.Name = "dgvTipoEleccion";
+            this.dgvTipoEleccion.Size = new System.Drawing.Size(667, 173);
+            this.dgvTipoEleccion.TabIndex = 120;
+            this.dgvTipoEleccion.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvTipoEleccion_CellContentClick);
+            // 
+            // TipoEleccion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.dgvTipoEleccion);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.txtTipoEleccion);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "TipoEleccion";
+            this.Text = "TipoEleccion";
+            this.Load += new System.EventHandler(this.TipoEleccion_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTipoEleccion)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label txtTipoEleccion;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.DataGridView dgvTipoEleccion;
+    }
+}
---
+++ TipoEleccion.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using MyLibreriaVoto;
+
+namespace grupo3_Proyecto
+{
+    public partial class TipoEleccion : Form
+    {
+
+        int idSeleccionado = 0;
+        public TipoEleccion()
+        {
+            InitializeComponent();
+        }
+
+        private void TipoEleccion_Load(object sender, EventArgs e)
+        {
+            CargarTipoEleccion();
+        }
+
+        void CargarTipoEleccion()
+        {
+            DataSet ds = Utilidades.ejecutar("SELECT * FROM TipoEleccion");
+            dgvTipoEleccion.DataSource = ds.Tables[0];
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtDescripcion.Text))
+            {
+                MessageBox.Show("Debe ingresar una descripción");
+                return;
+            }
+
+            Utilidades.ejecutarAccion(
+                "INSERT INTO TipoEleccion (Descripcion) VALUES (@Descripcion)",
+                new SqlParameter("@Descripcion", txtDescripcion.Text)
+            );
+
+            MessageBox.Show("Registro guardado correctamente");
+
+            CargarTipoEleccion();
+            Limpiar();
+        }
+
+        private void dgvTipoEleccion_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow fila = dgvTipoEleccion.Rows[e.RowIndex];
+
+                idSeleccionado = Convert.ToInt32(fila.Cells["IdTipoEleccion"].Value);
+                txtDescripcion.Text = fila.Cells["Descripcion"].Value.ToString();
+            }
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            if (idSeleccionado == 0)
+            {
+                MessageBox.Show("Seleccione un registro");
+                return;
+            }
+
+            Utilidades.ejecutarAccion(
+                "UPDATE TipoEleccion SET Descripcion = @Descripcion WHERE IdTipoEleccion = @Id",
+                new SqlParameter("@Descripcion", txtDescripcion.Text),
+                new SqlParameter("@Id", idSeleccionado)
+            );
+
+            MessageBox.Show("Registro actualizado");
+
+            CargarTipoEleccion();
+            Limpiar();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (idSeleccionado == 0)
+            {
+                MessageBox.Show("Seleccione un registro");
+                return;
+            }
+
+            Utilidades.ejecutarAccion(
+                "DELETE FROM TipoEleccion WHERE IdTipoEleccion = @Id",
+                new SqlParameter("@Id", idSeleccionado)
+            );
+
+            MessageBox.Show("Registro eliminado");
+
+            CargarTipoEleccion();
+            Limpiar();
+        }
+
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+
+        void Limpiar()
+        {
+            txtDescripcion.Clear();
+            idSeleccionado = 0;
+        }
+
+
+    }
+}
