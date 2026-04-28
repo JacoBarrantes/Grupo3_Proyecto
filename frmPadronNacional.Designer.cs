f7ab730b968cb41ba0b1ef9b3e619de087655c01
Nazareth Jimenez <nazareth.jimenez.godinez@gmail.com>
2026-04-27 22:39:06 -0600

    Cambios padron nacional

--- frmPadronNacional.Designer.cs
+++ frmPadronNacional.Designer.cs
@@ -47,93 +47,103 @@
             this.txtApellido1 = new System.Windows.Forms.TextBox();
             this.txtApellido2 = new System.Windows.Forms.TextBox();
             this.lblFechaVencimientoCed = new System.Windows.Forms.Label();
+            this.txtCrearUsuario = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvPadron)).BeginInit();
             this.SuspendLayout();
             // 
             // lblCedula
             // 
             this.lblCedula.AutoSize = true;
-            this.lblCedula.Location = new System.Drawing.Point(24, 28);
+            this.lblCedula.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCedula.Location = new System.Drawing.Point(26, 87);
             this.lblCedula.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblCedula.Name = "lblCedula";
-            this.lblCedula.Size = new System.Drawing.Size(40, 13);
+            this.lblCedula.Size = new System.Drawing.Size(59, 20);
             this.lblCedula.TabIndex = 0;
-            this.lblCedula.Text = "Cedula";
+            this.lblCedula.Text = "Cédula";
             // 
             // lblCodigoElectoral
             // 
             this.lblCodigoElectoral.AutoSize = true;
-            this.lblCodigoElectoral.Location = new System.Drawing.Point(188, 28);
+            this.lblCodigoElectoral.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCodigoElectoral.Location = new System.Drawing.Point(207, 87);
             this.lblCodigoElectoral.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblCodigoElectoral.Name = "lblCodigoElectoral";
-            this.lblCodigoElectoral.Size = new System.Drawing.Size(84, 13);
+            this.lblCodigoElectoral.Size = new System.Drawing.Size(125, 20);
             this.lblCodigoElectoral.TabIndex = 1;
             this.lblCodigoElectoral.Text = "Codigo Electoral";
             // 
             // lblJunta
             // 
             this.lblJunta.AutoSize = true;
-            this.lblJunta.Location = new System.Drawing.Point(592, 101);
+            this.lblJunta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblJunta.Location = new System.Drawing.Point(649, 157);
             this.lblJunta.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblJunta.Name = "lblJunta";
-            this.lblJunta.Size = new System.Drawing.Size(33, 13);
+            this.lblJunta.Size = new System.Drawing.Size(49, 20);
             this.lblJunta.TabIndex = 2;
             this.lblJunta.Text = "Junta";
             // 
             // lblNombre
             // 
             this.lblNombre.AutoSize = true;
-            this.lblNombre.Location = new System.Drawing.Point(24, 101);
+            this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(26, 160);
             this.lblNombre.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblNombre.Name = "lblNombre";
-            this.lblNombre.Size = new System.Drawing.Size(44, 13);
+            this.lblNombre.Size = new System.Drawing.Size(65, 20);
             this.lblNombre.TabIndex = 3;
             this.lblNombre.Text = "Nombre";
             // 
             // lblApellido1
             // 
             this.lblApellido1.AutoSize = true;
-            this.lblApellido1.Location = new System.Drawing.Point(188, 101);
+            this.lblApellido1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblApellido1.Location = new System.Drawing.Point(221, 160);
             this.lblApellido1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblApellido1.Name = "lblApellido1";
-            this.lblApellido1.Size = new System.Drawing.Size(53, 13);
+            this.lblApellido1.Size = new System.Drawing.Size(78, 20);
             this.lblApellido1.TabIndex = 4;
             this.lblApellido1.Text = "Apellido 1";
             // 
             // lblApellido2
             // 
             this.lblApellido2.AutoSize = true;
-            this.lblApellido2.Location = new System.Drawing.Point(403, 101);
+            this.lblApellido2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblApellido2.Location = new System.Drawing.Point(436, 160);
             this.lblApellido2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblApellido2.Name = "lblApellido2";
-            this.lblApellido2.Size = new System.Drawing.Size(53, 13);
+            this.lblApellido2.Size = new System.Drawing.Size(78, 20);
             this.lblApellido2.TabIndex = 5;
             this.lblApellido2.Text = "Apellido 2";
             // 
             // dtpVencimiento
             // 
-            this.dtpVencimiento.Location = new System.Drawing.Point(544, 27);
+            this.dtpVencimiento.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpVencimiento.Location = new System.Drawing.Point(629, 87);
             this.dtpVencimiento.Margin = new System.Windows.Forms.Padding(2);
             this.dtpVencimiento.Name = "dtpVencimiento";
-            this.dtpVencimiento.Size = new System.Drawing.Size(190, 20);
+            this.dtpVencimiento.Size = new System.Drawing.Size(190, 22);
             this.dtpVencimiento.TabIndex = 6;
             this.dtpVencimiento.ValueChanged += new System.EventHandler(this.dtpVencimientoCedula_ValueChanged);
             // 
             // dgvPadron
             // 
+            this.dgvPadron.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
             this.dgvPadron.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvPadron.Location = new System.Drawing.Point(17, 126);
+            this.dgvPadron.Location = new System.Drawing.Point(30, 202);
             this.dgvPadron.Margin = new System.Windows.Forms.Padding(2);
             this.dgvPadron.Name = "dgvPadron";
             this.dgvPadron.RowHeadersWidth = 51;
             this.dgvPadron.RowTemplate.Height = 24;
-            this.dgvPadron.Size = new System.Drawing.Size(716, 171);
+            this.dgvPadron.Size = new System.Drawing.Size(789, 144);
             this.dgvPadron.TabIndex = 7;
             this.dgvPadron.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvPadron_CellContentClick);
             // 
             // btnEliminar
             // 
-            this.btnEliminar.Location = new System.Drawing.Point(624, 325);
+            this.btnEliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.Location = new System.Drawing.Point(709, 374);
             this.btnEliminar.Margin = new System.Windows.Forms.Padding(2);
             this.btnEliminar.Name = "btnEliminar";
             this.btnEliminar.Size = new System.Drawing.Size(110, 39);
@@ -144,7 +154,8 @@
             // 
             // btnNuevo
             // 
-            this.btnNuevo.Location = new System.Drawing.Point(17, 325);
+            this.btnNuevo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNuevo.Location = new System.Drawing.Point(30, 374);
             this.btnNuevo.Margin = new System.Windows.Forms.Padding(2);
             this.btnNuevo.Name = "btnNuevo";
             this.btnNuevo.Size = new System.Drawing.Size(110, 39);
@@ -155,7 +166,8 @@
             // 
             // btnGuardar
             // 
-            this.btnGuardar.Location = new System.Drawing.Point(209, 325);
+            this.btnGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.Location = new System.Drawing.Point(568, 374);
             this.btnGuardar.Margin = new System.Windows.Forms.Padding(2);
             this.btnGuardar.Name = "btnGuardar";
             this.btnGuardar.Size = new System.Drawing.Size(110, 39);
@@ -166,7 +178,8 @@
             // 
             // btnModificar
             // 
-            this.btnModificar.Location = new System.Drawing.Point(422, 325);
+            this.btnModificar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnModificar.Location = new System.Drawing.Point(167, 374);
             this.btnModificar.Margin = new System.Windows.Forms.Padding(2);
             this.btnModificar.Name = "btnModificar";
             this.btnModificar.Size = new System.Drawing.Size(110, 39);
@@ -177,74 +190,94 @@
             // 
             // txtCedula
             // 
-            this.txtCedula.Location = new System.Drawing.Point(66, 28);
+            this.txtCedula.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCedula.Location = new System.Drawing.Point(89, 86);
             this.txtCedula.Margin = new System.Windows.Forms.Padding(2);
             this.txtCedula.Name = "txtCedula";
-            this.txtCedula.Size = new System.Drawing.Size(108, 20);
+            this.txtCedula.Size = new System.Drawing.Size(108, 26);
             this.txtCedula.TabIndex = 12;
             this.txtCedula.TextChanged += new System.EventHandler(this.txtCedula_TextChanged);
             // 
             // txtCodigoElectoral
             // 
-            this.txtCodigoElectoral.Location = new System.Drawing.Point(275, 24);
+            this.txtCodigoElectoral.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCodigoElectoral.Location = new System.Drawing.Point(340, 87);
             this.txtCodigoElectoral.Margin = new System.Windows.Forms.Padding(2);
             this.txtCodigoElectoral.Name = "txtCodigoElectoral";
-            this.txtCodigoElectoral.Size = new System.Drawing.Size(108, 20);
+            this.txtCodigoElectoral.Size = new System.Drawing.Size(108, 26);
             this.txtCodigoElectoral.TabIndex = 13;
             this.txtCodigoElectoral.TextChanged += new System.EventHandler(this.txtCodigoElectoral_TextChanged);
             // 
             // txtJunta
             // 
-            this.txtJunta.Location = new System.Drawing.Point(626, 96);
+            this.txtJunta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtJunta.Location = new System.Drawing.Point(711, 154);
             this.txtJunta.Margin = new System.Windows.Forms.Padding(2);
             this.txtJunta.Name = "txtJunta";
-            this.txtJunta.Size = new System.Drawing.Size(108, 20);
+            this.txtJunta.Size = new System.Drawing.Size(108, 26);
             this.txtJunta.TabIndex = 14;
             this.txtJunta.TextChanged += new System.EventHandler(this.txtJunta_TextChanged);
             // 
             // txtNombre
             // 
-            this.txtNombre.Location = new System.Drawing.Point(70, 96);
+            this.txtNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombre.Location = new System.Drawing.Point(99, 157);
             this.txtNombre.Margin = new System.Windows.Forms.Padding(2);
             this.txtNombre.Name = "txtNombre";
-            this.txtNombre.Size = new System.Drawing.Size(108, 20);
+            this.txtNombre.Size = new System.Drawing.Size(108, 26);
             this.txtNombre.TabIndex = 15;
             this.txtNombre.TextChanged += new System.EventHandler(this.txtNombre_TextChanged);
             // 
             // txtApellido1
             // 
-            this.txtApellido1.Location = new System.Drawing.Point(243, 96);
+            this.txtApellido1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtApellido1.Location = new System.Drawing.Point(312, 155);
             this.txtApellido1.Margin = new System.Windows.Forms.Padding(2);
             this.txtApellido1.Name = "txtApellido1";
-            this.txtApellido1.Size = new System.Drawing.Size(108, 20);
+            this.txtApellido1.Size = new System.Drawing.Size(108, 26);
             this.txtApellido1.TabIndex = 16;
             this.txtApellido1.TextChanged += new System.EventHandler(this.txtApellido1_TextChanged);
             // 
             // txtApellido2
             // 
-            this.txtApellido2.Location = new System.Drawing.Point(458, 96);
+            this.txtApellido2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtApellido2.Location = new System.Drawing.Point(528, 155);
             this.txtApellido2.Margin = new System.Windows.Forms.Padding(2);
             this.txtApellido2.Name = "txtApellido2";
-            this.txtApellido2.Size = new System.Drawing.Size(108, 20);
+            this.txtApellido2.Size = new System.Drawing.Size(108, 26);
             this.txtApellido2.TabIndex = 17;
             this.txtApellido2.TextChanged += new System.EventHandler(this.txtApellido2_TextChanged);
             // 
             // lblFechaVencimientoCed
             // 
             this.lblFechaVencimientoCed.AutoSize = true;
-            this.lblFechaVencimientoCed.Location = new System.Drawing.Point(406, 31);
+            this.lblFechaVencimientoCed.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFechaVencimientoCed.Location = new System.Drawing.Point(465, 90);
             this.lblFechaVencimientoCed.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblFechaVencimientoCed.Name = "lblFechaVencimientoCed";
-            this.lblFechaVencimientoCed.Size = new System.Drawing.Size(134, 13);
+            this.lblFechaVencimientoCed.Size = new System.Drawing.Size(150, 20);
             this.lblFechaVencimientoCed.TabIndex = 18;
-            this.lblFechaVencimientoCed.Text = "Fecha Vencimiento Cedula";
+            this.lblFechaVencimientoCed.Text = "Fecha Vencimiento ";
+            // 
+            // txtCrearUsuario
+            // 
+            this.txtCrearUsuario.AutoSize = true;
+            this.txtCrearUsuario.Font = new System.Drawing.Font("Modern No. 20", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCrearUsuario.Location = new System.Drawing.Point(269, 25);
+            this.txtCrearUsuario.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.txtCrearUsuario.Name = "txtCrearUsuario";
+            this.txtCrearUsuario.Size = new System.Drawing.Size(271, 36);
+            this.txtCrearUsuario.TabIndex = 115;
+            this.txtCrearUsuario.Text = "Padrón Nacional";
             // 
             // frmPadronNacional
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(774, 366);
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(858, 450);
             this.ControlBox = false;
+            this.Controls.Add(this.txtCrearUsuario);
             this.Controls.Add(this.lblFechaVencimientoCed);
             this.Controls.Add(this.txtApellido2);
             this.Controls.Add(this.txtApellido1);
@@ -298,5 +331,6 @@
         private System.Windows.Forms.TextBox txtApellido1;
         private System.Windows.Forms.TextBox txtApellido2;
         private System.Windows.Forms.Label lblFechaVencimientoCed;
+        private System.Windows.Forms.Label txtCrearUsuario;
     }
 }
