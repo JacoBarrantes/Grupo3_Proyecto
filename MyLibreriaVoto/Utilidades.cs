a96bb0c97be107eee674142ff27b57d0bbc393ee
Kevin Javier Leon Alvarado <kleona@edu.uc.ac.cr>
2026-04-28 14:44:15 -0600

    graficos formulario

--- MyLibreriaVoto/Utilidades.cs
+++ MyLibreriaVoto/Utilidades.cs
@@ -10,8 +10,8 @@ namespace MyLibreriaVoto
 {
     public class Utilidades
     {
-       
-        private static readonly string _cnn = @"Data Source=Nazareth;Initial Catalog=SistemaVotacion2026_2030;Integrated Security=True;";
+                                               //Data Source=Kevin-Leon\MSSSQLSERVER2026;Initial Catalog=SistemaVotacion2026_2030;Integrated Security=True;
+        private static readonly string _cnn = @"Data Source=Kevin-Leon\MSSSQLSERVER2026;Initial Catalog=SistemaVotacion2026_2030;Integrated Security=True;
         public static DataSet ejecutar(string comando)
         {
             using (SqlConnection conn = new SqlConnection(_cnn))
---
+++ graficos.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace grupo3_Proyecto
+{
+    partial class frmGraficos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbltitulo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbltitulo
+            // 
+            this.lbltitulo.AutoSize = true;
+            this.lbltitulo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(51)))), ((int)(((byte)(102)))));
+            this.lbltitulo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lbltitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltitulo.ForeColor = System.Drawing.Color.White;
+            this.lbltitulo.Location = new System.Drawing.Point(0, 0);
+            this.lbltitulo.Name = "lbltitulo";
+            this.lbltitulo.Size = new System.Drawing.Size(460, 25);
+            this.lbltitulo.TabIndex = 6;
+            this.lbltitulo.Text = "Sistema Electoral - Graficos y Estadisticas";
+            this.lbltitulo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // frmGraficos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbltitulo);
+            this.Name = "frmGraficos";
+            this.Text = "Graficos del Sistema";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbltitulo;
+    }
+}
